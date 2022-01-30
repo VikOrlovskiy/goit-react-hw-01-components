c574d4ea751e539d39eec7b303ad22bea0e3f1ba
vik_orel <vikorel34@gmail.com>
2022-01-30 10:20:43 +0200

    error

--- src/TransactionHistory/TransactionHistory.js
+++ src/TransactionHistory/TransactionHistory.js
@@ -1,24 +1,28 @@
-import PropTypes from 'prop-types';
-import TransactionTableItem from './TransactionTableItem '
+import PropTypes from "prop-types"
+// import TransactionTableItem from './TransactionTableItem '
 
-export default function TransactionTable ({items}) {
-  return  <table className="transaction-history">
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
-  {items.map(item =>
-    <tbody key = {item.id}>
-        <TransactionTableItem 
-        type ={item.type}
-        amount ={item.amount} 
-        currency ={item.currency}/>
-    </tbody>  )}
-</table>
+export default function TransactionTable({ items }) {
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      {items.map((item) => (
+        <tbody key={item.id}>
+          <TransactionTableItem
+            type={item.type}
+            amount={item.amount}
+            currency={item.currency}
+          />
+        </tbody>
+      ))}
+    </table>
+  )
 }
 TransactionTable.propTypes = {
-    key: PropTypes.string,
-  }
+  key: PropTypes.string,
+}
